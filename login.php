6f6be9e54013f8069305d8f8bc2aa79326a5e0ad
James Shearin <jamesjshearin@gmail.com>
2019-11-05 21:27:39 -0800

    added more PHP

--- login.php
+++ login.php
@@ -1,16 +1,26 @@
 
 
 <html en="us">
+
+<head>
     <meta charset="UTF-8">
     <title>Login Result</title>
+    <link href="functions.php"
+</head>
+
 <body>
 
-You're
+<h1><?php echo $_GET["username"]?></h1>
+
+You're username is
 <?php
 echo $_GET["username"];
-echo $_GET["password"];
 ?>
-
+<br>
+You're password is
+<?php
+echo $_GET["password"]
+?>
 
 </body>
 </html>
